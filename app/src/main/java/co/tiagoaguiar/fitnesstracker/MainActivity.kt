596c0e34e2dde86f8af1069308beb103db3e24be
Stenio Sants <stenio.neo@gmail.com>
2023-06-08 01:11:17 -0300

    inserindo lougin e segunda page

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -1,11 +1,22 @@
 package co.tiagoaguiar.fitnesstracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+private lateinit var btnLoguin: Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnLoguin = findViewById(R.id.btn_loguin)
+        btnLoguin.setOnClickListener {
+            val i = Intent(this, MainActivity2::class.java)
+            startActivity(i)
+        }
     }
 }
